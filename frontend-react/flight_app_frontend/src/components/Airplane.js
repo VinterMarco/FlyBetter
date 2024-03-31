2b8a8b30408cbbd931e0af0b913dab428bcfe0ab
Vinter Marco <vintermarco@192.168.1.18>
2024-03-31 17:21:08 +0300

    plane beta added

---
+++ frontend-react/flight_app_frontend/src/components/Airplane.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+const Airplane = () => {
+    return (
+        <div style={{height : '90vh'}}>
+            <div style={{height: '100%', backgroundColor : 'yellow'}}>
+            <div style={{
+                background: 'lightgrey',
+                height : '100%',
+                width : '40%',
+                margin : 'auto',
+                borderTopLeftRadius : '240px',
+                borderTopRightRadius : '240px',
+            }} className="wrapper">
+
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Airplane;
--- frontend-react/flight_app_frontend/src/routes/Bookings.js
+++ frontend-react/flight_app_frontend/src/routes/Bookings.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React from 'react';
+import Airplane from "../components/Airplane";
+
+
+
 
 
 const Bookings = () => {
-    return <h1>Bookings</h1>;
+
+    return (
+        <div style={{display: 'flex'}}>
+            <div style={{backgroundColor: 'red', width : '50%'}}>
+                <Airplane/>
+            </div>
+            <div style={{backgroundColor: 'green',width : '50%'}}>form</div>
+        </div>
+    );
 };
 
 export default Bookings;
